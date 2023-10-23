ecb3f5171f7a52557a8a26356f4bb73b7d354be8
Julian Rolon <j.rolont@uniandes.edu.co>
2023-10-23 02:22:40 -0500

    Taller 1

--- tsBasico/Serie.ts
+++ tsBasico/Serie.ts
@@ -1,14 +1,22 @@
-class Serie {
-    constructor(
-      public id: number,
-      public title: string,
-      public channel: string,
-      public seassons: number,
-      public description: string,
-      public program: string,
-      public images: string
-    ) {}
+export class Serie {
+    
+  public id: number;
+  public title: string;
+  public channel: string;
+  public seasons: number;
+  public description: string;
+  public program: string;
+  public images: string;
+    
+  
+  constructor(id: number, title: string, channel: string, seasons: number, description: string, program: string, images: string){
+    this.id = id;
+    this.title = title;
+    this.channel = channel;
+    this.seasons = seasons;
+    this.description = description;
+    this.program = program; 
+    this.images = images;
   }
-  
-  export default Serie;
-  
+
+}
--- tsBasico/main.ts
+++ tsBasico/main.ts
@@ -1,1 +1,31 @@
-document.getElementsByTagName("h1")[0].innerHTML = "Hola desde Typescript";
+
+
+import { Serie } from './serie.js';
+import { dataSeries } from './data.js';
+
+let seriesTbody: HTMLElement = document.getElementById('series')!;
+const avgSeasonsElm: HTMLElement = document.getElementById("avg-seasons")!;
+
+renderSeriesInTable(dataSeries);
+
+avgSeasonsElm.innerHTML = `${getAvgSeasons(dataSeries)}`
+
+function renderSeriesInTable(series: Series[]): void {
+    console.log('Desplegando series');
+    series.forEach(s => {
+        let trElement = document.createElement("tr");
+        trElement.innerHTML = `<td>${s.id}</td>
+                           <td>${s.name}</td>
+                           <td>${s.channel}</td>
+                           <td>${s.seasons}</td>`;
+        seriesTbody.appendChild(trElement);
+  });
+}
+
+function getAvgSeasons(series: Series[]): number {
+  let totalSeasons: number = 0;
+  series.forEach((series) => totalSeasons = totalSeasons + series.seasons);
+  let seasonsAvg: number = totalSeasons / series.length 
+  return seasonsAvg;
+}
+          
