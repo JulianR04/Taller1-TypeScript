e15b91eca30f7a4648b6167c7a3964b71aee4918
JulianR04 <111070690+JulianR04@users.noreply.github.com>
2023-10-23 01:26:06 -0500

    Add files via upload

---
+++ tsBasico/Serie.ts
@@ -0,0 +1,14 @@
+class Serie {
+    constructor(
+      public id: number,
+      public title: string,
+      public channel: string,
+      public seassons: number,
+      public description: string,
+      public program: string,
+      public images: string
+    ) {}
+  }
+  
+  export default Serie;
+  
---
+++ tsBasico/serie.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Serie {
+    constructor(id, title, channel, seassons, description, program, images) {
+        this.id = id;
+        this.title = title;
+        this.channel = channel;
+        this.seassons = seassons;
+        this.description = description;
+        this.program = program;
+        this.images = images;
+    }
+}
+const _default = Serie;
+export { _default as default };
